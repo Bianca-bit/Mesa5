a52a46645242ac83cca979a6f2dafc03a420f9d1
Bianca <bialisilva@hotmail.com>
2023-02-28 22:52:08 -0300

    Add existing file

---
+++ script_aula5.js
@@ -0,0 +1,11 @@
+// let h1 = document.querySelector("h1");
+// console.log(h1);
+// h1.style.color = "#401179";
+// h1.style.textAlign = "center";
+
+function acionou(){
+    alert("clicou")
+
+    let body = document.querySelector("body");
+    body.classList.toggle("modedark");
+}
