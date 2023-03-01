8f5a8212287713b1a1d807d2618f42140f40e408
Bianca <bialisilva@hotmail.com>
2023-03-01 19:05:39 -0300

    atualizado

--- script_aula5.js
+++ script_aula5.js
@@ -7,5 +7,15 @@ function acionou(){
     alert("clicou")
 
     let body = document.querySelector("body");
-    body.classList.toggle("modedark");
-}
+    console.log(body);
+    body.style.backgroundColor = "rgb(19, 19, 19)"
+
+    let h1 = document.querySelector("h1");
+    console.log(h1);
+    h1.style.backgroundColor = "rgb(19, 19, 19)"
+    h1.style.color = "rgb(170, 170, 170)";
+    h1.style.borderColor = "black";
+
+    let item = document.querySelector("item");
+    item.divClass.toggle("mode-dark");   
+ }
