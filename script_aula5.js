b13d215d47ea3044f1a99329f288886eeffc88e8
Bianca <bialisilva@hotmail.com>
2023-03-01 23:18:19 -0300

    atualizado

--- script_aula5.js
+++ script_aula5.js
@@ -3,19 +3,40 @@
 // h1.style.color = "#401179";
 // h1.style.textAlign = "center";
 
-function acionou(){
-    alert("clicou")
+/* function acionou(){
+     alert("clicou")
 
-    let body = document.querySelector("body");
-    console.log(body);
-    body.style.backgroundColor = "rgb(19, 19, 19)"
+     let body = document.querySelector("body");
+     console.log(body);
+     body.style.backgroundColor = "rgb(19, 19, 19)"
 
-    let h1 = document.querySelector("h1");
-    console.log(h1);
-    h1.style.backgroundColor = "rgb(19, 19, 19)"
-    h1.style.color = "rgb(170, 170, 170)";
-    h1.style.borderColor = "black";
+     let h1 = document.querySelector("h1");
+     console.log(h1);
+     h1.style.backgroundColor = "rgb(19, 19, 19)"
+     h1.style.color = "rgb(170, 170, 170)";
+     h1.style.borderColor = "black";
 
-    let item = document.querySelector("item");
-    item.divClass.toggle("mode-dark");   
- }
+     let item  = document.querySelector("item");
+     console.log(h1);
+
+     item.style.backgroundColor = "rgb(19, 19, 19)"
+     h1.style.color = "rgb(170, 170, 170)";
+     h1.style.borderColor = "black";
+   
+}*/
+
+let body = document.querySelector("body");
+         
+let h1 = document.querySelector("h1");
+
+let cards = document.querySelectorAll(".item")
+
+function alteraTemaComToggle(){
+    body.classList.toggle("bodydark");
+    h1.classList.toggle("darkmode1");
+
+    for(let card of cards){
+        card.classList.toggle("darkmode2");
+        card.classList.toggle("darkmode3");
+    }
+}
